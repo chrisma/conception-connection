2762b7b50b688b5be8f01095fa19048f522a4f1a
Christoph Matthies <christoph.matthies@gmail.com>
2014-09-08 23:31:21 +0200

    First code.

---
+++ wiki_event.js
@@ -0,0 +1,56 @@
+var fetchEvent = function(date, variance, callback) {
+
+	// Helpers
+	var monthNames = [ "January", "February", "March", "April", "May", "June",
+	"July", "August", "September", "October", "November", "December" ];
+	function constructWikiTitle(d) {
+		return monthNames[d.getMonth()] + '_' + d.getDate();
+	}
+	function addDays(date, days) {
+		var result = new Date(date);
+		result.setDate(date.getDate() + days);
+		return result;
+	}
+
+	// Construct Wikipedia article titles
+	var titles = [];
+	titles.push(constructWikiTitle(date));
+	for ( var i = 1; i < variance+1; i++ ) {
+		// 1 day variance means one day ahead, one day before
+		titles.push(constructWikiTitle(addDays(date, i)));
+		titles.push(constructWikiTitle(addDays(date, i*-1)));
+	}
+
+	// Build the Wikipedia query
+	titles = titles.join('|');
+	var wikiEndpoint = 'http://en.wikipedia.org/w/api.php';
+	var url = wikiEndpoint + '?format=json&action=query&prop=revisions' + 
+		'&rvprop=content&rvparse&rvsection=1&callback=?&titles=' + titles;
+
+	// Retrieve and parse the data
+	$.getJSON( url, function(data) {
+		var result = [];
+		var pages = data.query.pages;
+		// The keys in the returned JSON are page IDs
+		for (var pageId in pages) {
+			if (pages.hasOwnProperty(pageId)) {
+				var page = pages[pageId];
+				var content = page.revisions[0]['*'];
+				var element = $('<div>').html(content);
+				var lines = element.children('ul').first().children('li');
+				lines.each( function(index){
+					var splitLine = $(this).text().split('–');
+					var eventYear = splitLine[0].trim();
+					if (date.getFullYear() == eventYear) { // need to convert to string in comparison
+						var titleSplit = page.title.split(' ');
+						var eventDate = new Date(eventYear, monthNames.indexOf(titleSplit[0]), titleSplit[1]);
+						var eventDescription = splitLine.slice(1).join('–').trim();
+						result.push({date:eventDate, description:eventDescription});
+					}
+				});
+			}
+		}
+		callback(result);
+	});
+
+};
